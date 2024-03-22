ae627ab5cfe1a460aade5ae5fb920e751b8c1b3f
Ashutosh Gauniyal <aga124@uclive.ac.nz>
2024-03-22 22:36:12 +1300

    added package.json files to git

---
+++ user_types.d.ts
@@ -0,0 +1,29 @@
+type User ={
+    /**
+     * User id defined by the database 
+     */
+    id: number,
+    /**
+     * user's email as entered when created 
+     */
+    email: string,
+    /**
+     * user's firstname as entered when created 
+     */
+    first_name: string,
+    /**
+     * user's lastname as entered when created 
+     */
+    last_name:string,
+    /**
+     * user's lastname as entered when created 
+     */
+    password: string
+     /**
+     * user's lastname as entered when created 
+     */
+    auth_token: string
+
+
+
+}
